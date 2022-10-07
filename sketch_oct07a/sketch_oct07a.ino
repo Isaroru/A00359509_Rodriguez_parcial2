7fea25faac466ca85b2aeeed2859fc8e26e0c0a1
Isabella Rodríguez <81999502+Isaroru@users.noreply.github.com>
2022-10-07 08:57:21 -0500

    Add files via upload

---
+++ sketch_oct07a/sketch_oct07a.ino
@@ -0,0 +1,42 @@
+const int LEDS[5] = {4,5,6,7,8};
+int buzz_pin = 3;
+int hum_pin = A0;
+int humValue = 0;
+int mapValue = 0;
+
+void setup(){
+  Serial.begin(9600);
+  for(int i= 0; i < 5; i ++){
+    pinMode(LEDS[i], OUTPUT);
+  }
+  pinMode(buzz_pin, OUTPUT);
+  pinMode(hum_pin, INPUT);  
+}
+
+void loop(){
+  humValue = analogRead(hum_pin);
+  Serial.println(humValue);
+  Serial.print(' ');
+  Serial.println(mapValue);
+  Serial.print(' ');
+  
+  mapValue = map(humValue, 0, 876, 0, 100);
+  
+  if(mapValue <= 20){
+    tone(buzz_pin, 1000, 100);
+    delay(100);
+  }
+  
+  if(mapValue >= 80){
+    tone(buzz_pin, 2000, 1000);
+  }
+  
+  if(mapValue = 100){
+    for(int i= 0; i < 5; i ++){
+      //mapValue = map(humValue, 0, 876, 0, 100);
+      digitalWrite(LEDS[i], HIGH);
+        }else if(mapValue < 100){
+          digitalWrite(LEDS[i], LOW);
+      }
+    } 
+}
